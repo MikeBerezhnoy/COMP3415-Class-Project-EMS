af85a2d49a6199d30abc6b4a25be4e2be84c949d
MikeBerezhnoy <mberezhn@lakeheadu.ca>
2023-11-27 13:47:20 -0500

    Moved db info to Config file

---
+++ config/global.js
@@ -0,0 +1,9 @@
+//configurations file
+//contains all configuration data for the application
+
+const configurations = {
+    'db': 'mongodb+srv://hrosebor:cAk0yqChpTD5ZOp1@cluster0.njk7204.mongodb.net/EMS',
+    'sessionSecret': 'EMSSuperDuperSecret',
+}
+ 
+module.exports = configurations;
